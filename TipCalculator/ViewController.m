afd10676ee2f7ba558b79ef79900d214670de39d
Dan <dan.christal@gmail.com>
2016-09-16 11:36:22 -0700

    basic tip calculator

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipDisplayLabel;
 
 @end
 
@@ -24,6 +27,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)tapRecieved:(UITapGestureRecognizer *)sender {
+    [self.billAmountTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+    
+}
 
+- (IBAction)calculateTip:(UIButton *)sender {
+    
+    
+    NSNumber *tipPercent;
+    NSNumber *billAmount;
+    NSNumber *tipResult;
+    
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    numberFormatter.alwaysShowsDecimalSeparator = NO;
+    numberFormatter.usesSignificantDigits = YES;
+    numberFormatter.maximumSignificantDigits = 3;
+    numberFormatter.minimumSignificantDigits = 1;
+    
+    
+    
+    tipPercent = [numberFormatter numberFromString:self.tipPercentageTextField.text];
+    billAmount = [numberFormatter numberFromString:self.billAmountTextField.text];
+    
+    tipResult = @([billAmount integerValue] * ([tipPercent integerValue]*0.01));
+    
+    self.tipDisplayLabel.text = [@"$" stringByAppendingString:[numberFormatter stringFromNumber:tipResult]];
+    
+    
+}
 
 @end
